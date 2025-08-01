8e0df7a33b5d004ade94509d59c436483a67aa93
andrea-twl <tan.wanling.andrea@gmail.com>
2025-08-01 09:39:47 +0000

    add: organised skeleton code

---
+++ utils/.ipynb_checkpoints/PATHS-checkpoint.py
@@ -0,0 +1,8 @@
+import os
+
+HOME_DIR = "/home/jovyan"
+PROJECT_DIR = os.path.join(HOME_DIR, "arc-sg")
+
+REPO_DIR = os.path.abspath("..")
+DATA_DIR = os.path.join(PROJECT_DIR, "data")
+OUTPUT_DIR = os.path.join(PROJECT_DIR, "outputs")
---
+++ utils/.ipynb_checkpoints/data_utils-checkpoint.py
@@ -0,0 +1,21 @@
+LAB_FIELDS = {
+    "ID": "Patient ID",
+    "RACE": "Race",
+    "DOB": "Date of Birth",
+    "NATIONALITY": "Nationality",
+    "INSTITUTION": "Institution Code",
+    "TEST_NAME": "Lab Resulted Order Test Description",
+    "RESULT": "Result Value",
+    "DATE": "Specimen Received Date" # "Specimen Collection Date",
+}
+
+DIAG_FIELDS = {
+    "ID": "Patient ID",
+    "SEX": "Gender",
+    "RACE": "Race",
+    "DOB": "Date of Birth",
+    "Nationality": "Nationality",
+    "INSTITUTION": "Institution Code",
+    "ICD_10": "Diagnosis Code (ICD10)",
+    "DIAG_DATE": "Diagnosis Date",
+}
